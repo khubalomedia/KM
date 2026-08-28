971a2851c6134233ab610820759bb48e03c29dbc
khubalomedia <mortalitymotha@gmail.com>
2026-08-28 19:55:38 +0200

    Uploud my file on website
    Link fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const playlists = {
   educational: [
 
     {
-      videoId: "8UWAlJm-SKI",
+      videoId: "8YR3BxEqegRg",
       title: "From TikTok To Fame, Lasizwe Dambuza, Uncle Waffles, Wian Van Den Berg"
     },
 
